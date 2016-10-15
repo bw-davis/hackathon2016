ff110a413ec75370636b812d72c92c510c3c2329
Brian Davis <bwd@uoregon.edu>
2016-10-15 08:50:06 -0700

    first commit

---
+++ Hackathon2016/Hackathon2016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hackathon2016
+//
+//  Created by Mac Owner on 10/15/16.
+//  Copyright © 2016 Mac Owner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
