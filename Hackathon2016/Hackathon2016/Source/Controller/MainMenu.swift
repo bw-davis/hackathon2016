0adca4d016cd173a1d3858512f3fde193913e5e9
Brian Davis <bwd@uoregon.edu>
2016-10-15 10:43:08 -0700

    created initial view

---
+++ Hackathon2016/Hackathon2016/Source/Controller/MainMenu.swift
@@ -0,0 +1,16 @@
+//
+//  MainMenu.swift
+//  Hackathon2016
+//
+//  Created by Mac Owner on 10/15/16.
+//  Copyright © 2016 Mac Owner. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+    }
+}
